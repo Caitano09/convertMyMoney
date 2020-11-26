d5db3551bb39031a01acdc12677b86f88ad52205
Caitano <danielcaitano9@hotmail.com>
2020-11-26 16:48:07 -0300

    folder coverage added

--- public/javascript.js
+++ public/javascript.js
@@ -14,7 +14,7 @@ $(function () {
 $("#form").submit(function() {
     if($("#campo").val()== null || $("#campo").val() ==""){
         alert('campo vazio');
-        return false;
+        return false;//
     }
 });
 */
